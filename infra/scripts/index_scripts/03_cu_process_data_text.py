37eae388b36f31bd748b7d450c2833d6ed6637d3
Pavan-Microsoft <v-kupavan@microsoft.com>
2025-03-05 17:12:59 +0530

    till sql connection

--- infra/scripts/index_scripts/03_cu_process_data_text.py
+++ infra/scripts/index_scripts/03_cu_process_data_text.py
@@ -45,117 +45,117 @@ print("search_endpoint: ",search_endpoint)
 print("deployment: ",deployment)
 print("managed_identity_client_id: ",managed_identity_client_id)
 
-# # Function: Get Embeddings 
-# def get_embeddings(text: str,openai_api_base,openai_api_version,openai_api_key):
-#     model_id = "text-embedding-ada-002"
-#     client = AzureOpenAI(
-#         api_version=openai_api_version,
-#         azure_endpoint=openai_api_base,
-#         api_key = openai_api_key
-#     )
+# Function: Get Embeddings 
+def get_embeddings(text: str,openai_api_base,openai_api_version,openai_api_key):
+    model_id = "text-embedding-ada-002"
+    client = AzureOpenAI(
+        api_version=openai_api_version,
+        azure_endpoint=openai_api_base,
+        api_key = openai_api_key
+    )
     
-#     embedding = client.embeddings.create(input=text, model=model_id).data[0].embedding
+    embedding = client.embeddings.create(input=text, model=model_id).data[0].embedding
 
-#     return embedding
+    return embedding
 
-# # Function: Clean Spaces with Regex - 
-# def clean_spaces_with_regex(text):
-#     # Use a regular expression to replace multiple spaces with a single space
-#     cleaned_text = re.sub(r'\s+', ' ', text)
-#     # Use a regular expression to replace consecutive dots with a single dot
-#     cleaned_text = re.sub(r'\.{2,}', '.', cleaned_text)
-#     return cleaned_text
+# Function: Clean Spaces with Regex - 
+def clean_spaces_with_regex(text):
+    # Use a regular expression to replace multiple spaces with a single space
+    cleaned_text = re.sub(r'\s+', ' ', text)
+    # Use a regular expression to replace consecutive dots with a single dot
+    cleaned_text = re.sub(r'\.{2,}', '.', cleaned_text)
+    return cleaned_text
 
-# def chunk_data(text):
-#     tokens_per_chunk = 1024 #500
-#     text = clean_spaces_with_regex(text)
-#     SENTENCE_ENDINGS = [".", "!", "?"]
-#     WORDS_BREAKS = ['\n', '\t', '}', '{', ']', '[', ')', '(', ' ', ':', ';', ',']
+def chunk_data(text):
+    tokens_per_chunk = 1024 #500
+    text = clean_spaces_with_regex(text)
+    SENTENCE_ENDINGS = [".", "!", "?"]
+    WORDS_BREAKS = ['\n', '\t', '}', '{', ']', '[', ')', '(', ' ', ':', ';', ',']
 
-#     sentences = text.split('. ') # Split text into sentences
-#     chunks = []
-#     current_chunk = ''
-#     current_chunk_token_count = 0
+    sentences = text.split('. ') # Split text into sentences
+    chunks = []
+    current_chunk = ''
+    current_chunk_token_count = 0
     
-#     # Iterate through each sentence
-#     for sentence in sentences:
-#         # Split sentence into tokens
-#         tokens = sentence.split()
+    # Iterate through each sentence
+    for sentence in sentences:
+        # Split sentence into tokens
+        tokens = sentence.split()
         
-#         # Check if adding the current sentence exceeds tokens_per_chunk
-#         if current_chunk_token_count + len(tokens) <= tokens_per_chunk:
-#             # Add the sentence to the current chunk
-#             if current_chunk:
-#                 current_chunk += '. ' + sentence
-#             else:
-#                 current_chunk += sentence
-#             current_chunk_token_count += len(tokens)
-#         else:
-#             # Add current chunk to chunks list and start a new chunk
-#             chunks.append(current_chunk)
-#             current_chunk = sentence
-#             current_chunk_token_count = len(tokens)
+        # Check if adding the current sentence exceeds tokens_per_chunk
+        if current_chunk_token_count + len(tokens) <= tokens_per_chunk:
+            # Add the sentence to the current chunk
+            if current_chunk:
+                current_chunk += '. ' + sentence
+            else:
+                current_chunk += sentence
+            current_chunk_token_count += len(tokens)
+        else:
+            # Add current chunk to chunks list and start a new chunk
+            chunks.append(current_chunk)
+            current_chunk = sentence
+            current_chunk_token_count = len(tokens)
     
-#     # Add the last chunk
-#     if current_chunk:
-#         chunks.append(current_chunk)
+    # Add the last chunk
+    if current_chunk:
+        chunks.append(current_chunk)
     
-#     return chunks
+    return chunks
 
-# from azure.search.documents import SearchClient
-# from azure.storage.filedatalake import (
-#     DataLakeServiceClient
-# )
+from azure.search.documents import SearchClient
+from azure.storage.filedatalake import (
+    DataLakeServiceClient
+)
 
-# account_name =  get_secrets_from_kv(key_vault_name, "ADLS-ACCOUNT-NAME")
+account_name =  get_secrets_from_kv(key_vault_name, "ADLS-ACCOUNT-NAME")
 
-# account_url = f"https://{account_name}.dfs.core.windows.net"
+account_url = f"https://{account_name}.dfs.core.windows.net"
 
-# credential = DefaultAzureCredential(managed_identity_client_id=managed_identity_client_id)
-# print("credential: ",credential)
-# print("account_url: ",account_url)
-# service_client = DataLakeServiceClient(account_url, credential=credential,api_version='2023-01-03') 
+credential = DefaultAzureCredential(managed_identity_client_id=managed_identity_client_id)
+print("credential: ",credential)
+print("account_url: ",account_url)
+service_client = DataLakeServiceClient(account_url, credential=credential,api_version='2023-01-03') 
 
-# file_system_client = service_client.get_file_system_client(file_system_client_name)  
-# directory_name = directory
-# print("directory_name: ",directory_name)
-# paths = file_system_client.get_paths(path=directory_name)
-# print(paths)
+file_system_client = service_client.get_file_system_client(file_system_client_name)  
+directory_name = directory
+print("directory_name: ",directory_name)
+paths = file_system_client.get_paths(path=directory_name)
+print(paths)
 
-# index_name = "call_transcripts_index"
+index_name = "call_transcripts_index"
 
-# from azure.search.documents.indexes import SearchIndexClient
+from azure.search.documents.indexes import SearchIndexClient
 
-# search_credential = AzureKeyCredential(search_key)
+search_credential = AzureKeyCredential(search_key)
 
-# search_client = SearchClient(search_endpoint, index_name, search_credential)
-# index_client = SearchIndexClient(endpoint=search_endpoint, credential=search_credential)
+search_client = SearchClient(search_endpoint, index_name, search_credential)
+index_client = SearchIndexClient(endpoint=search_endpoint, credential=search_credential)
 
-# driver = "{ODBC Driver 18 for SQL Server}"
-# server =  get_secrets_from_kv(key_vault_name,"SQLDB-SERVER")
-# database = get_secrets_from_kv(key_vault_name,"SQLDB-DATABASE")
-# print("server: ",server)
-# credential = DefaultAzureCredential(managed_identity_client_id=managed_identity_client_id)
+driver = "{ODBC Driver 18 for SQL Server}"
+server =  get_secrets_from_kv(key_vault_name,"SQLDB-SERVER")
+database = get_secrets_from_kv(key_vault_name,"SQLDB-DATABASE")
+print("server: ",server)
+credential = DefaultAzureCredential(managed_identity_client_id=managed_identity_client_id)
 
-# token_bytes = credential.get_token(
-#     "https://database.windows.net/.default"
-# ).token.encode("utf-16-LE")
-# token_struct = struct.pack(f"<I{len(token_bytes)}s", len(token_bytes), token_bytes)
-# SQL_COPT_SS_ACCESS_TOKEN = (
-#     1256  # This connection option is defined by microsoft in msodbcsql.h
-# )
+token_bytes = credential.get_token(
+    "https://database.windows.net/.default"
+).token.encode("utf-16-LE")
+token_struct = struct.pack(f"<I{len(token_bytes)}s", len(token_bytes), token_bytes)
+SQL_COPT_SS_ACCESS_TOKEN = (
+    1256  # This connection option is defined by microsoft in msodbcsql.h
+)
 
-# # Set up the connection
-# connection_string = f"DRIVER={driver};SERVER={server};DATABASE={database};"
-# conn = pyodbc.connect(
-#     connection_string, attrs_before={SQL_COPT_SS_ACCESS_TOKEN: token_struct}
-# )
+# Set up the connection
+connection_string = f"DRIVER={driver};SERVER={server};DATABASE={database};"
+conn = pyodbc.connect(
+    connection_string, attrs_before={SQL_COPT_SS_ACCESS_TOKEN: token_struct}
+)
 
-# # conn = pymssql.connect(server, username, password, database)
-# cursor = conn.cursor()
-# print("Connected to the database")
-# cursor.execute('DROP TABLE IF EXISTS processed_data')
-# conn.commit()
+# conn = pymssql.connect(server, username, password, database)
+cursor = conn.cursor()
+print("Connected to the database")
+cursor.execute('DROP TABLE IF EXISTS processed_data')
+conn.commit()
 
 # create_processed_data_sql = """CREATE TABLE processed_data (
 #                 ConversationId varchar(255) NOT NULL PRIMARY KEY,
