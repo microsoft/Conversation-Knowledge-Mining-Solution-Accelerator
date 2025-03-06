e09bc1db29187c519d1b7111fd76e01bd2445f8d
Rohini-Microsoft <v-rwalunj@microsoft.com>
2025-03-06 16:18:44 +0530

    changes added relation to docs

--- src/App/frontend/src/components/Citations/AnswerParser.tsx
+++ src/App/frontend/src/components/Citations/AnswerParser.tsx
@@ -12,6 +12,10 @@ let filteredCitations = [] as Citation[];
 
 // Define a function to check if a citation with the same Chunk_Id already exists in filteredCitations
 const isDuplicate = (citation: Citation,citationIndex:string) => {
+    console.log("citation::",citation);
+    console.log("citationIndex 16::",citationIndex);
+    console.log("filteredCitations 17::",filteredCitations);
+    console.log("filteredCitations.some((c) => c.chunk_id === citation.chunk_id)::", filteredCitations.some((c) => c.chunk_id === citation.chunk_id))
     return filteredCitations.some((c) => c.chunk_id === citation.chunk_id) ;
 };
 
@@ -26,13 +30,17 @@ export function parseAnswer(answer: AskResponse): ParsedAnswer {
     citationLinks?.forEach(link => {
         // Replacing the links/citations with number
         let citationIndex = link.slice(lengthDocN, link.length - 1);
+        console.log("citationIndex::",citationIndex);
         let citation = cloneDeep(answer.citations[Number(citationIndex) - 1]) as Citation;
-
-        if (citation !== undefined && !isDuplicate(citation, citationIndex)) {
+        console.log("citation::",citation);
+        if (citation !== undefined ) { //&& !isDuplicate(citation, citationIndex)
+            console.log("inside::");
           answerText = answerText.replaceAll(link, ` ^${++citationReindex}^ `);
           citation.id = citationIndex; // original doc index to de-dupe
           citation.reindex_id = citationReindex.toString(); // reindex from 1 for display
+          console.log("citationReindex::",citationReindex);
           filteredCitations.push(citation);
+        
         }else{
             // Replacing duplicate citation with original index
             let matchingCitation = filteredCitations.find((ct) => citation?.chunk_id == ct?.chunk_id);
@@ -50,3 +58,8 @@ export function parseAnswer(answer: AskResponse): ParsedAnswer {
         markdownFormatText:  answerText
     };
 }
+// 3,4,1,3
+// [3,4,1]
+// 3,3,4,4,3,1,4,1
+//3,1,4,4,3,1,4,1
+// []
