9cf485ffb49eb29561a8eb24dc12e8dfc2bbdd3a
Pavan-Microsoft <v-kupavan@microsoft.com>
2025-03-08 00:46:48 +0530

    Loop through users and assign roles dynamically

--- infra/scripts/run_all_deployment_scripts.sh
+++ infra/scripts/run_all_deployment_scripts.sh
@@ -49,6 +49,26 @@ curl -s -o /scripts/create-sql-user-and-role.ps1 ${createSqlUserAndRoleScriptsUr
 chmod +x /scripts/create-sql-user-and-role.ps1
 
 # Execute SQL scripts for users and roles
-pwsh -File /scripts/create-sql-user-and-role.ps1 -SqlServerName ${sqlServerName} -SqlDatabaseName ${sqlDbName} -ClientId ${sqlUsers[0].principalId} -DisplayName ${sqlUsers[0].principalName} -ManagedIdentityClientId ${managedIdentityClientId} -DatabaseRole ${sqlUsers[0].databaseRoles[0]} 
-pwsh -File /scripts/create-sql-user-and-role.ps1 -SqlServerName ${sqlServerName} -SqlDatabaseName ${sqlDbName} -ClientId ${sqlUsers[0].principalId} -DisplayName ${sqlUsers[0].principalName} -ManagedIdentityClientId ${managedIdentityClientId} -DatabaseRole ${sqlUsers[0].databaseRoles[1]}
-pwsh -File /scripts/create-sql-user-and-role.ps1 -SqlServerName ${sqlServerName} -SqlDatabaseName ${sqlDbName} -ClientId ${sqlUsers[1].principalId} -DisplayName ${sqlUsers[1].principalName} -ManagedIdentityClientId ${managedIdentityClientId} -DatabaseRole ${sqlUsers[1].databaseRoles[0]}
+# Parse sqlUsers JSON
+sqlUsersJson=$(echo "$sqlUsers" | jq -c '.')
+
+# Loop through users and assign roles dynamically
+userCount=$(echo "$sqlUsersJson" | jq '. | length')
+for i in $(seq 0 $((userCount - 1))); do
+    principalId=$(echo "$sqlUsersJson" | jq -r ".[$i].principalId")
+    principalName=$(echo "$sqlUsersJson" | jq -r ".[$i].principalName")
+    
+    # Loop through roles for each user
+    roleCount=$(echo "$sqlUsersJson" | jq ".[$i].databaseRoles | length")
+    for j in $(seq 0 $((roleCount - 1))); do
+        databaseRole=$(echo "$sqlUsersJson" | jq -r ".[$i].databaseRoles[$j]")
+
+        pwsh -File /scripts/create-sql-user-and-role.ps1 \
+            -SqlServerName "${sqlServerName}" \
+            -SqlDatabaseName "${sqlDbName}" \
+            -ClientId "${principalId}" \
+            -DisplayName "${principalName}" \
+            -ManagedIdentityClientId "${managedIdentityClientId}" \
+            -DatabaseRole "${databaseRole}"
+    done
+done
