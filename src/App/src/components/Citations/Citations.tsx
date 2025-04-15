198e005ca56514f6638837a04663e5d3194c5d63
Rohini-Microsoft <v-rwalunj@microsoft.com>
2025-04-15 10:59:52 +0530

    added logic for onclick on citation link api call

--- src/App/src/components/Citations/Citations.tsx
+++ src/App/src/components/Citations/Citations.tsx
@@ -4,6 +4,7 @@ import { useAppContext } from '../../state/useAppContext';
 import { actionConstants } from '../../state/ActionConstants';
 import "./Citations.css";
 import { AskResponse, Citation } from '../../types/AppTypes';
+import { fetchCitationContent } from '../../api/api';
 
 interface Props {
     answer: AskResponse;
@@ -27,12 +28,21 @@ const Citations = ({ answer, index }: Props) => {
         return citationFilename;
     };
 
-    const onCitationClicked = (
+    const onCitationClicked = async (
         citation: Citation
-    ) => {
+    ) => {  
+        // let citationv = {
+        //     "content": "",
+        //     "url": "https://kmpnqiikor537m-search.search.windows.net/indexes/call_transcripts_index/docs/5b8e9e1f-04d4-4f0d-bae6-fc4f8af9486f_01?api-version=2024-07-01&$select=id,content",
+        //     "title": "doc_2"
+        // }
+        const citationContent = await fetchCitationContent(citation);//(citation.url);
+
+        // console.log("Fetched citation content", citationContent)
+        // console.log("Citation clicked", citation);
         dispatch({
             type: actionConstants.UPDATE_CITATION,
-            payload: { showCitation: true, activeCitation: citation },
+            payload: { showCitation: true, activeCitation: {...citation, content:citationContent.content.content} },
         });
     };
 
