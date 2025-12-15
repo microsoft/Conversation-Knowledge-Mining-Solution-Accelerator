86ffcf03a6c74e51793d92222ddcb21e58376ac0
Pavan-Microsoft <v-kupavan@microsoft.com>
2025-12-15 16:14:22 +0530

    feat: Remove branch parameter from azd init command in install script

--- infra/vscode_web/install.sh
+++ infra/vscode_web/install.sh
@@ -1,3 +1,3 @@
 pip install -r requirements.txt --user -q
 
-azd init -t microsoft/Conversation-Knowledge-Mining-Solution-Accelerator -b pk-km-sampledata-manual
+azd init -t microsoft/Conversation-Knowledge-Mining-Solution-Accelerator
