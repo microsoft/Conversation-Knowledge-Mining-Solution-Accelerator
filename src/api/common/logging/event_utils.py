ac4c996311d25117ec047b2b4aab655a691c828d
Abdul-Microsoft <v-amujeebta@microsoft.com>
2026-03-19 12:52:57 +0530

    fix: Change log level from debug to warning for unconfigured Application Insights tracking

--- src/api/common/logging/event_utils.py
+++ src/api/common/logging/event_utils.py
@@ -22,4 +22,4 @@ def track_event_if_configured(event_name: str, event_data: dict):
     if instrumentation_key:
         track_event(event_name, event_data)
     else:
-        logger.debug("Skipping track_event for %s: Application Insights is not configured", event_name)
+        logger.warning("Skipping track_event for %s: Application Insights is not configured", event_name)
